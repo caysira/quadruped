016b016c04365e7a0c634fb4626739f78f2bfd24
caysira <caysira@users.noreply.github.com>
2014-10-22 10:59:14 +0200

    Update calibrate_servos.ino
    small cosmetic changes

--- tools/calibrate_servos.ino
+++ tools/calibrate_servos.ino
@@ -1,10 +1,8 @@
-/***************************************************
-  Arduino sketch for servo calibration (min/max positions)
-  for usage with Adafruit ServoShield-Library (12 bit pwm)
-   
-  the user should enter servo number (0-15) and 
-  position (pwm-high duration 0..4096)
-****************************************************/
+//  Arduino sketch for servo calibration (min/max positions)
+//  for usage with Adafruit ServoShield-Library (12 bit pwm)
+//
+//  the user should enter servo number (0-15) and 
+//  position (pwm-high duration 0..4096)
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
