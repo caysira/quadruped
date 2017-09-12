6361ba6542efe0b5bd216823bf5985eb5fe6a380
caysira <caysira@users.noreply.github.com>
2017-09-12 18:09:14 +0200

    Update basic_moves.ino
    started refactoring of variable names and defines to increase readability of the code

--- tools/basic_moves/basic_moves.ino
+++ tools/basic_moves/basic_moves.ino
@@ -7,51 +7,56 @@
 #include <Adafruit_PWMServoDriver.h>
 
 // define servo names for "knees"
-#define K_FL 6    // knee front left
-#define K_FR 8    // knee front right
-#define K_RL 4    // knee rear left
-#define K_RR 2    // knee rear right
+#define KNEE_FRONT_LEFT 6
+#define KNEE_FRONT_RIGHT 8
+#define KNEE_REAR_LEFT 4
+#define KNEE_REAR_RIGHT 2
+
 // knees up positions
-#define U_K_FL 110    
-#define U_K_FR 450    
-#define U_K_RL 110    
-#define U_K_RR 450    
+#define POS_UP_KNEE_FRONT_LEFT 110    
+#define POS_UP_KNEE_FRONT_RIGHT 450    
+#define POS_UP_KNEE_REAR_LEFT 110    
+#define POS_UP_KNEE_REAR_RIGHT 450    
+
 // knees flat positions
-#define F_K_FL 180   
-#define F_K_FR 370     
-#define F_K_RL 180     
-#define F_K_RR 370     
+#define POS_FLAT_KNEE_FRONT_LEFT 180   
+#define POS_FLAT_KNEE_FRONT_REAR 370     
+#define POS_FLAT_KNEE_REAR_LEFT 180     
+#define POS_FLAT_KNEE_REAR_RIGHT 370
+
 // knees down positions
-#define D_K_FL 430    
-#define D_K_FR 140     
-#define D_K_RL 430     
-#define D_K_RR 140     
-// knees base positions
-#define B_K_FL 330    
-#define B_K_FR 230     
-#define B_K_RL 330     
-#define B_K_RR 230     
+#define POS_DOWN_KNEE_FRONT_LEFT 430    
+#define POS_DOWN_KNEE_FRONT_RIGHT 140     
+#define POS_DOWN_KNEE_REAR_LEFT 430     
+#define POS_DOWN_KNEE_REAR_RIGHT 140     
+
+// knees base positions (also initial starting position)
+#define POS_BASE_KNEE_FRONT_LEFT 330    
+#define POS_BASE_KNEE_FRONT_RIGHT 230     
+#define POS_BASE_KNEE_REAR_LEFT 330     
+#define POS_BASE_KNEE_REAR_RIGHT 230     
 
 // define servo names for "hips"
-#define H_FL 5    // hip front left
-#define H_FR 7    // hip front right
-#define H_RL 3    // hip rear left
-#define H_RR 1    // hip rear right
+#define HIP_FRONT_LEFT 5
+#define HIP_FRONT_RIGHT 7
+#define HIP_REAR_LEFT 3
+#define HIP_REAR_RIGHT 1
+
 // hips base position
-#define B_H_FL 270    
-#define B_H_FR 290     
-#define B_H_RL 270     
-#define B_H_RR 210 
+#define POS_BASE_HIP_FRONT_LEFT 270    
+#define POS_BASE_HIP_FRON_REAR 290     
+#define POS_BASE_HIP_REAR_LEFT 270     
+#define POS_BASE_HIP_REAR_RIGHT 210 
 
 // define min/max value (resolution) for relative positioning
 #define REL_MIN 0
 #define REL_MAX 100
 
 // relative position values of base position for knees
-#define B_K_FL_REL 31
-#define B_K_FR_REL 29
-#define B_K_RL_REL 31
-#define B_K_RR_REL 29
+#define POS_REL_BASE_KNEE_FRONT_LEFT 31
+#define POS_REL_BASE_KNEE_FRONT_REAR 29
+#define POS_REL_BASE_KNEE_REAR_LEFT 31
+#define POS_REL_BASE_KNEE_REAR_RIGHT 29
 
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
@@ -66,38 +71,47 @@ void setup() {
 
 // sets the positions of the hip-servos to the respective positions
 // the values are raw-values as excpeted ba pws.setPWM
-void knees_raw(int raw_fl, int raw_fr, int raw_rl, int raw_rr) {
-  pwm.setPWM(K_FL, 0, raw_fl);
-  pwm.setPWM(K_FR, 0, raw_fr);
-  pwm.setPWM(K_RL, 0, raw_rl);
-  pwm.setPWM(K_RR, 0, raw_rr);
+void knees_raw(int raw_front_left,
+               int raw_front_right,
+               int raw_rear_left,
+               int raw_rear_right) {
+  pwm.setPWM(KNEE_FRONT_LEFT, 0, raw_front_left);
+  pwm.setPWM(KNEE_FRONT_RIGHT, 0, raw_front_right);
+  pwm.setPWM(KNEE_REAR_LEFT, 0, raw_rear_left);
+  pwm.setPWM(KNEE_REAR_RIGHT, 0, raw_rear_right);
 }
 
 // sets the positions of the shoulder-servos to the respective positions
 // the values are raw-values as excpeted ba pws.setPWM
-void hips_raw(int raw_fl, int raw_fr, int raw_rl, int raw_rr) {
-  pwm.setPWM(H_FL, 0, raw_fl);
-  pwm.setPWM(H_FR, 0, raw_fr);
-  pwm.setPWM(H_RL, 0, raw_rl);
-  pwm.setPWM(H_RR, 0, raw_rr);
+void hips_raw(int raw_front_left,
+              int raw_front_right,
+              int raw_rear_left,
+              int raw_rear_right) {
+  pwm.setPWM(HIP_FRONT_LEFT, 0, raw_front_left);
+  pwm.setPWM(HIP_FRONT_RIGHT, 0, raw_front_right);
+  pwm.setPWM(HIP_REAR_LEFT, 0, raw_rear_left);
+  pwm.setPWM(HIP_REAR_RIGHT, 0, raw_rear_right);
 }
 
 // sets the positions for the hip-servos to the desired positions
 // this are relative positions mapped to the raw ones accordingly
 // the position range goes from REL_MIN..REL_MAX: 
 // REL_MIN=leg fully down, REL_MAX=leg fully up
-void knees_pos(int pos_fl, int pos_fr, int pos_rl, int pos_rr) {
+void knees_pos(int pos_front_left,
+               int pos_front_right,
+               int pos_rear_left,
+               int pos_rear_right) {
 
-  int r_fl, r_fr, r_rl, r_rr;
+  int raw_front_left, raw_front_right, raw_rear_left, raw_rear_right;
   
   // map positions values to raw values for pwm.setPWM
-  r_fl = map(pos_fl, REL_MIN, REL_MAX, D_K_FL, U_K_FL);
-  r_fr = map(pos_fr, REL_MIN, REL_MAX, D_K_FR, U_K_FR);
-  r_rl = map(pos_rl, REL_MIN, REL_MAX, D_K_RL, U_K_RL);
-  r_rr = map(pos_rr, REL_MIN, REL_MAX, D_K_RR, U_K_RR);
+  raw_front_left = map(pos_front_left, REL_MIN, REL_MAX, POS_DOWN_KNEE_FRONT_LEFT, POS_UP_KNEE_FRONT_LEFT);
+  raw_front_right = map(pos_front_right, REL_MIN, REL_MAX, POS_DOWN_KNEE_FRONT_RIGHT, POS_UP_KNEE_FRONT_RIGHT);
+  raw_rear_left = map(pos_rear_left, REL_MIN, REL_MAX, POS_DOWN_KNEE_REAR_LEFT, POS_UP_KNEE_REAR_LEFT);
+  raw_rear_right = map(pos_rear_right, REL_MIN, REL_MAX, POS_DOWN_KNEE_REAR_RIGHT, POS_UP_KNEE_REAR_RIGHT);
   
   // set servos to desired positions
-  knees_raw(r_fl, r_fr, r_rl, r_rr);
+  knees_raw(raw_front_left, raw_front_right, raw_rear_left, raw_rear_right);
   
 }
 
@@ -105,14 +119,14 @@ void loop() {
   
   delay(2000); // always a good idea to wait a moment ;-)
   // then go to start position
-  hips_raw(B_H_FL, B_H_FR, B_H_RL, B_H_RR); 
-  knees_raw(B_K_FL, B_K_FR, B_K_RL, B_K_RR);
+  hips_raw(POS_BASE_HIP_FRONT_LEFT, POS_BASE_HIP_FRONT_RIGHT, POS_BASE_HIP_REAR_LEFT, POS_BASE_HIP_REAR_RIGHT); 
+  knees_raw(POS_BASE_KNEE_FRONT_LEFT, POS_BASE_KNEE_FRONT_RIGHT, POS_BASE_KNEE_REAR_LEFT, POS_BASE_KNEE_REAR_RIGHT);
   delay(2000);
   
   // now let's try the relative positioning
   // robot should lie down flat slowly, starting from base position
   int pos_inc = 0;
-  while ((B_K_FL_REL + pos_inc) <= REL_MAX) {
+  while ((POS_REL_BASE_KNEE_FRONT_LEFT + pos_inc) <= REL_MAX) {
     knees_pos(B_K_FL_REL + pos_inc, 
               B_K_FR_REL + pos_inc, 
               B_K_RL_REL + pos_inc, 
