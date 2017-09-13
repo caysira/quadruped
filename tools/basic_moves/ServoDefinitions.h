177321e683794e74ea26488294b9d63e94f19451
caysira <caysira@users.noreply.github.com>
2017-09-13 14:51:46 +0200

    Create ServoDefinitions.h
    put Servo Definitions in ow file

---
+++ tools/basic_moves/ServoDefinitions.h
@@ -0,0 +1,50 @@
+// define servo names for "knees"
+#define KNEE_FRONT_LEFT 6
+#define KNEE_FRONT_RIGHT 8
+#define KNEE_REAR_LEFT 4
+#define KNEE_REAR_RIGHT 2
+
+// knees up positions
+#define POS_UP_KNEE_FRONT_LEFT 110
+#define POS_UP_KNEE_FRONT_RIGHT 450
+#define POS_UP_KNEE_REAR_LEFT 110
+#define POS_UP_KNEE_REAR_RIGHT 450
+
+// knees flat positions
+#define POS_FLAT_KNEE_FRONT_LEFT 180
+#define POS_FLAT_KNEE_FRONT_REAR 370
+#define POS_FLAT_KNEE_REAR_LEFT 180
+#define POS_FLAT_KNEE_REAR_RIGHT 370
+
+// knees down positions
+#define POS_DOWN_KNEE_FRONT_LEFT 430
+#define POS_DOWN_KNEE_FRONT_RIGHT 140
+#define POS_DOWN_KNEE_REAR_LEFT 430
+#define POS_DOWN_KNEE_REAR_RIGHT 140
+
+// knees base positions (also initial starting position)
+#define POS_BASE_KNEE_FRONT_LEFT 330
+#define POS_BASE_KNEE_FRONT_RIGHT 230
+#define POS_BASE_KNEE_REAR_LEFT 330
+#define POS_BASE_KNEE_REAR_RIGHT 230
+
+// define servo names for "hips"
+#define HIP_FRONT_LEFT 5
+#define HIP_FRONT_RIGHT 7
+#define HIP_REAR_LEFT 3
+#define HIP_REAR_RIGHT 1
+
+// hips base position
+#define POS_BASE_HIP_FRONT_LEFT 270
+#define POS_BASE_HIP_FRON_REAR 290
+#define POS_BASE_HIP_REAR_LEFT 270
+#define POS_BASE_HIP_REAR_RIGHT 210 
+
+// define min/max value (resolution) for relative positioning
+#define REL_MIN 0#define REL_MAX 100
+
+// relative position values of base position for knees
+#define POS_REL_BASE_KNEE_FRONT_LEFT 31
+#define POS_REL_BASE_KNEE_FRONT_REAR 29
+#define POS_REL_BASE_KNEE_REAR_LEFT 31
+#define POS_REL_BASE_KNEE_REAR_RIGHT 29
